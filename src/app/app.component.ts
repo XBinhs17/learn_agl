3a275534c889511e8722dc946751b23adfeb30fb
XBinhs17 <nguyenbinh17062003@gmail.com>
2025-04-13 19:42:59 +0700

    two-way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderLayoutComponent } from "./shared/header-layout/header-layout.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderLayoutComponent],
+  imports: [RouterOutlet, HeaderLayoutComponent,  FormsModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   nameBtn = 'click me!';
   clickMessage = '';
+
+  bindingMessage = '';
 
   handleClickMe(): void{
     this.clickMessage = 'Button clicked!';
