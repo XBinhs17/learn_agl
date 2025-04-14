2c8af5f35f97c8e92760ba24702dc8646beec50b
XBinhs17 <nguyenbinh17062003@gmail.com>
2025-04-14 16:40:47 +0700

    Http Mapping

--- src/app/Home/home.component.ts
+++ src/app/Home/home.component.ts
@@ -1,17 +1,15 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ProductItems } from '../shared/types/productItem';
-import { ProductItemComponent } from "../shared/product-item/productItem.component";
+import { ProductItemComponent } from '../shared/product-item/productItem.component';
 import { HttpClient } from '@angular/common/http';
 import { BlogService } from '../../services/BlogService';
-import { Subscription } from 'rxjs';
+import { map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
-  imports: [
-    ProductItemComponent
-],
+  imports: [ProductItemComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit, OnDestroy {
   nameBtn = 'click me!';
@@ -24,52 +22,82 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   getBlogApi: Subscription;
 
-
   products: ProductItems[] = [
-    {id: 1, name: 'product 1', price: 450000, image: 'https://res.cloudinary.com/dlteq4ism/image/upload/v1744352069/XB2_whitebg_ctaeq0.png'},
-    {id: 2, name: 'product 2', price: 550000, image: 'https://res.cloudinary.com/dlteq4ism/image/upload/v1744352069/XB2_whitebg_ctaeq0.png'},
-    {id: 3, name: 'product 3', price: 650000, image: 'https://res.cloudinary.com/dlteq4ism/image/upload/v1744352069/XB2_whitebg_ctaeq0.png'},
-    {id: 4, name: 'product 4', price: 750000, image: 'https://res.cloudinary.com/dlteq4ism/image/upload/v1744352069/XB2_whitebg_ctaeq0.png'}
+    {
+      id: 1,
+      name: 'product 1',
+      price: 450000,
+      image:
+        'https://res.cloudinary.com/dlteq4ism/image/upload/v1744352069/XB2_whitebg_ctaeq0.png',
+    },
+    {
+      id: 2,
+      name: 'product 2',
+      price: 550000,
+      image:
+        'https://res.cloudinary.com/dlteq4ism/image/upload/v1744352069/XB2_whitebg_ctaeq0.png',
+    },
+    {
+      id: 3,
+      name: 'product 3',
+      price: 650000,
+      image:
+        'https://res.cloudinary.com/dlteq4ism/image/upload/v1744352069/XB2_whitebg_ctaeq0.png',
+    },
+    {
+      id: 4,
+      name: 'product 4',
+      price: 750000,
+      image:
+        'https://res.cloudinary.com/dlteq4ism/image/upload/v1744352069/XB2_whitebg_ctaeq0.png',
+    },
   ];
 
-  handleDelete = (id: number)=>{
-    const productIndex = this.products.findIndex(item => item.id == id);
+  handleDelete = (id: number) => {
+    const productIndex = this.products.findIndex((item) => item.id == id);
     if (productIndex !== -1) {
       this.products.splice(productIndex, 1);
     }
-  }
+  };
 
-  constructor(private blogService: BlogService){
+  constructor(private blogService: BlogService) {
     console.log('Initalize component');
     this.getBlogApi = new Subscription();
   }
 
   ngOnInit(): void {
-    this.getBlogApi = this.blogService.getBlogs().subscribe(({data}) => {
-      this.products = data.map((item:any) => {
-        return{
-          ...item,
-          name: item.title,
-          price: item.body,
-          image: 'https://res.cloudinary.com/dlteq4ism/image/upload/v1744352069/XB2_whitebg_ctaeq0.png',
-
-        }
+    this.getBlogApi = this.blogService
+      .getBlogs()
+      .pipe(
+        map(({ data }) =>
+          data.map((item: any) => {
+            return {
+              ...item,
+              name: item.title,
+              price: item.body,
+              image:
+                'https://res.cloudinary.com/dlteq4ism/image/upload/v1744352069/XB2_whitebg_ctaeq0.png',
+            };
+          }).filter(product => product.price > 300000)
+        )
+      )
+      .subscribe((res) => {
+        this.products = res;
       });
-    });
   }
 
   ngOnDestroy(): void {
-      if(this.getBlogApi){
-        this.getBlogApi.unsubscribe();
-        console.log('getBlogApi unsub');
-      }
+    if (this.getBlogApi) {
+      this.getBlogApi.unsubscribe();
+      console.log('getBlogApi unsub');
+    }
   }
 
-  handleClickMe(): void{
+  handleClickMe(): void {
     this.clickMessage = 'Button clicked!';
   }
 
-  updateField(): void{
+  updateField(): void {
     console.log('hello world');
   }
 }
