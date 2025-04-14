9116dfe422cd08dd0b07e68eed454aeed52d1e36
XBinhs17 <nguyenbinh17062003@gmail.com>
2025-04-14 13:35:02 +0700

    custom Routing

---
+++ src/app/Home/home.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderLayoutComponent } from "../shared/header-layout/header-layout.component";
+import { FormsModule } from '@angular/forms';
+import { currencyPipe } from '../shared/pipes/CurrencyPipe.pipe';
+import { upperCasePipe } from '../shared/pipes/UpperCasePipe.pipe';
+import { NgClass, NgFor, NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    RouterOutlet,
+    HeaderLayoutComponent,
+    FormsModule,
+    currencyPipe,
+    upperCasePipe,
+    NgFor,
+    NgIf
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  nameBtn = 'click me!';
+  clickMessage = '';
+
+  bindingMessage = '';
+
+  isActive = false;
+  isVisible = false;
+
+  products = [
+    {name: 'product 1', price: 450000, image: 'https://res.cloudinary.com/dlteq4ism/image/upload/v1744352069/XB2_whitebg_ctaeq0.png'},
+    {name: 'product 2', price: 550000, image: 'https://res.cloudinary.com/dlteq4ism/image/upload/v1744352069/XB2_whitebg_ctaeq0.png'},
+    {name: 'product 3', price: 650000, image: 'https://res.cloudinary.com/dlteq4ism/image/upload/v1744352069/XB2_whitebg_ctaeq0.png'},
+    {name: 'product 4', price: 750000, image: 'https://res.cloudinary.com/dlteq4ism/image/upload/v1744352069/XB2_whitebg_ctaeq0.png'}
+  ];
+
+  handleClickMe(): void{
+    this.clickMessage = 'Button clicked!';
+  }
+
+  updateField(): void{
+    console.log('hello world');
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,36 +11,10 @@ import { NgClass, NgFor, NgIf } from '@angular/common';
   imports: [
     RouterOutlet,
     HeaderLayoutComponent,
-    FormsModule,
-    currencyPipe,
-    upperCasePipe,
-    NgFor,
-    NgIf
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  nameBtn = 'click me!';
-  clickMessage = '';
 
-  bindingMessage = '';
-
-  isActive = false;
-  isVisible = false;
-
-  products = [
-    {name: 'product 1', price: 450000, image: 'https://res.cloudinary.com/dlteq4ism/image/upload/v1744352069/XB2_whitebg_ctaeq0.png'},
-    {name: 'product 2', price: 550000, image: 'https://res.cloudinary.com/dlteq4ism/image/upload/v1744352069/XB2_whitebg_ctaeq0.png'},
-    {name: 'product 3', price: 650000, image: 'https://res.cloudinary.com/dlteq4ism/image/upload/v1744352069/XB2_whitebg_ctaeq0.png'},
-    {name: 'product 4', price: 750000, image: 'https://res.cloudinary.com/dlteq4ism/image/upload/v1744352069/XB2_whitebg_ctaeq0.png'}
-  ];
-
-  handleClickMe(): void{
-    this.clickMessage = 'Button clicked!';
-  }
-
-  updateField(): void{
-    console.log('hello world');
-  }
 }
