83069aa6f85bed2d0b92c7903360d74a27c8f9e7
XBinhs17 <nguyenbinh17062003@gmail.com>
2025-04-14 11:14:13 +0700

    list render

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { HeaderLayoutComponent } from "./shared/header-layout/header-layout.comp
 import { FormsModule } from '@angular/forms';
 import { currencyPipe } from './shared/pipes/CurrencyPipe.pipe';
 import { upperCasePipe } from './shared/pipes/UpperCasePipe.pipe';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderLayoutComponent,  FormsModule, currencyPipe, upperCasePipe ],
+  imports: [RouterOutlet, HeaderLayoutComponent,  FormsModule, currencyPipe, upperCasePipe, NgFor ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -16,6 +17,13 @@ export class AppComponent {
   clickMessage = '';
 
   bindingMessage = '';
+
+  products = [
+    {name: 'product 1', price: 450000, image: 'https://res.cloudinary.com/dlteq4ism/image/upload/v1744352069/XB2_whitebg_ctaeq0.png'},
+    {name: 'product 2', price: 550000, image: 'https://res.cloudinary.com/dlteq4ism/image/upload/v1744352069/XB2_whitebg_ctaeq0.png'},
+    {name: 'product 3', price: 650000, image: 'https://res.cloudinary.com/dlteq4ism/image/upload/v1744352069/XB2_whitebg_ctaeq0.png'},
+    {name: 'product 4', price: 750000, image: 'https://res.cloudinary.com/dlteq4ism/image/upload/v1744352069/XB2_whitebg_ctaeq0.png'}
+  ];
 
   handleClickMe(): void{
     this.clickMessage = 'Button clicked!';
